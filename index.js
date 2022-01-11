fa4fe39a1fa17afa1568e9e696082d7aaefc8bc7
Kevin Gallo <Gallok731@gmail.com>
2022-01-11 10:34:51 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solution in this file!
+const hq = 42;
+const oneBlock = 264;
+function distanceFromHqInBlocks(pickupLocation) {
+    return Math.abs(42-pickupLocation);
+  }
+function distanceFromHqInFeet(pickupLocation) {
+    return Math.abs(distanceFromHqInBlocks(pickupLocation)*oneBlock);
+  }
+function distanceTravelledInFeet(start, destination) {
+    return Math.abs(distanceFromHqInFeet(destination)-distanceFromHqInFeet(start));
+  }
+function calculatesFarePrice(start, destination) {
+    let distanceInFeet = Math.abs(distanceFromHqInFeet(destination)-distanceFromHqInFeet(start));
+    if(distanceInFeet <= 400)
+    {
+        return 0;
+    }else if(distanceInFeet >= 401 && distanceInFeet < 2000)
+    {
+        return (distanceInFeet-400)*0.02;
+    }else if(distanceInFeet < 2500 && distanceInFeet > 2001)
+    {
+        return 25;
+    }else{
+        return 'cannot travel that far';
+    }
+  }
